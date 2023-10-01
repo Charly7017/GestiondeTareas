cac6e48871f7570f2792ac51b1d7bab059310a60
Charly <alexrace18@gmail.com>
2023-10-01 13:26:47 -0600

    Clase Tarea Completada

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -3,5 +3,11 @@
 	public class Tarea
 	{
 		public int Id { get; set; }
-	}
+        public string Nombre { get; set; }
+        public int TipoTareaId { get; set; }
+        public int Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+		public DateTime FechaLimite { get; set; }
+        public bool Completada { get; set; }
+    }
 }
